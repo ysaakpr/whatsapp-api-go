ac1cd28caeb27487e9e094ef47e10cef9ffd331a
Vyshakh P <ysaakpr@gmail.com>
2023-06-03 14:14:18 +0530

    reverted the json encoding change

--- webhook.go
+++ webhook.go
@@ -712,9 +712,8 @@ func (api *API) WebhookVerificationHandler(w http.ResponseWriter, r *http.Reques
 	if len(mode) > 0 && len(token) > 0 {
 		if mode == "subscribe" && token == api.WebHookVerification {
 			w.WriteHeader(http.StatusOK)
-			// jData, _ := json.Marshal(challenge)
-			// w.Write(jData)
-			w.Write([]byte(challenge))
+			jData, _ := json.Marshal(challenge)
+			w.Write(jData)
 		} else {
 			w.WriteHeader(http.StatusForbidden)
 		}
